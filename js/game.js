09a7ca0df0cd2e7e5f83859377a87e4f9c2ba6cf
ikoroleva <prsml.ellisson@gmail.com>
2022-05-27 00:38:55 +0200

    added classes

--- js/game.js
+++ js/game.js
@@ -1,148 +1,175 @@
-let gameStarted = false;
+class Game {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.isStarted = false;
 
-let heroX = 100;
-let heroY = 500;
-const hero = document.querySelector('.hero');
-hero.style.top = heroY + 'px';
-hero.style.left = heroX + 'px';
+    }
 
-let ghostX = 600;
-let ghostY = 200;
-const ghost = document.querySelector('.ghost');
-ghost.style.top = ghostY + 'px';
-ghost.style.left = ghostX + 'px';
+    start() {
+        this.isStarted = true;
+        const startBtn = document.querySelector('.start_btn');
+        const map = document.querySelector('.map');
+        map.removeChild(startBtn);
+    }
 
-let treasureX = 400;
-let treasureY = 0;
-const treasure = document.querySelector('.treasure');
-treasure.style.top = treasureY + 'px';
-treasure.style.left = treasureX + 'px';
+    render() {
+        const map = document.querySelector('.map');
 
-//const map = document.querySelector('.map');
-//console.log(map);
+        map.style.width = this.width + 'px';
+        map.style.height = this.height + 'px';
 
-const startGame = () => {
+        const startBtn = document.querySelector('.start_btn');
 
-    const startBtn = document.querySelector('.start_btn');
-    startBtn.parentNode.removeChild(startBtn);
+        startBtn.addEventListener('click', () => { this.start() });
+    }
 
-    gameStarted = true;
+    stop() {
+        this.isStarted = false;
+    }
+    checkWin(treasure, hero, gameInterval) {
+        if (treasure.x == hero.x && treasure.y == hero.y) {
+            const victory = document.querySelector('.victory');
+            victory.style.display = 'block';
+            this.stop();
+            clearInterval(gameInterval);
+        }
+    }
+
+    checkLose(ghost, hero, gameInterval) {
+        if (ghost.x == hero.x && ghost.y == hero.y) {
+            const defeat = document.querySelector('.defeat');
+            defeat.style.display = 'block';
+            game.stop();
+            clearInterval(gameInterval);
+        }
+    }
 
 }
 
-const moveHero = (e) => {
-    //console.log(e);
-    if (!gameStarted) { return; }
-
-    if ((e.key == 'w' || e.key == 'ArrowUp') && heroY > 0) {
-        heroY -= 100;
+class Hero {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.element = document.querySelector('.hero');
     }
 
-    if ((e.key == 's' || e.key == 'ArrowDown') && heroY < 500) {
-        heroY += 100;
+    render() {
+        this.element.style.top = this.y + 'px';
+        this.element.style.left = this.x + 'px';
     }
 
-    if ((e.key == 'a' || e.key == 'ArrowLeft') && heroX > 0) {
-        heroX -= 100;
+    move(key, game, treasure, gameInterval) {
+        if (!game.isStarted) { return; }
+        if ((key == 'w' || key == 'ArrowUp') && this.y > 0) {
+            this.y -= 100;
+        }
+        if ((key == 's' || key == 'ArrowDown') && this.y < 500) {
+            this.y += 100;
+        }
+        if ((key == 'a' || key == 'ArrowLeft') && this.x > 0) {
+            this.x -= 100;
+        }
+        if ((key == 'd' || key == 'ArrowRight') && this.x < 700) {
+            this.x += 100;
+        }
+        this.element.style.left = this.x + 'px';
+        this.element.style.top = this.y + 'px';
+        game.checkWin(treasure, this, gameInterval);
     }
-    if ((e.key == 'd' || e.key == 'ArrowRight') && heroX < 700) {
-        heroX += 100;
-    }
-    hero.style.left = heroX + 'px';
-    hero.style.top = heroY + 'px';
-
-    checkWin();
-    // console.log(ghostX);
-    // console.log(heroX);
 }
 
-// const moveAny = (who, whereX, whereY) => {
-//     if (who == 'hero') {
-//         hero.style.left = whereX * 100 + 'px';
-//         hero.style.top = whereY + 'px';
-//     }
-// }
+class Treasure {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.element = document.querySelector('.treasure');
+    }
 
-document.addEventListener('keyup', moveHero);
-document.addEventListener('click', startGame);
+    render() {
+        this.element.style.top = this.y + 'px';
+        this.element.style.left = this.x + 'px';
+    }
+}
 
+class Ghost {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.element = document.querySelector('.ghost');
+    }
+    render() {
+        this.element.style.top = this.y + 'px';
+        this.element.style.left = this.x + 'px';
+    }
+    moveToHero(hero) {
+        let moveX = 0;
+        let moveY = 0;
+        //move to hero by X
+        if (this.x > hero.x) {
+            moveX = -1;
+        }
+        else if (this.x < hero.x) {
+            moveX = 1;
+        }
+        else {
+            moveX = 0;
+        }
+        // move to hero by Y
+        if (this.y > hero.y) {
+            moveY = -1;
+        }
+        else if (this.y < hero.y) {
+            moveY = 1;
+        }
+        else {
+            moveY = 0;
+        }
+        // compare moveX and moveY
+        if (moveX == 0 && moveY != 0) {
+            this.y += moveY * 100;
+        }
+        else if (moveX != 0 && moveY == 0) {
+            this.x += moveX * 100;
+        }
+        else if (moveX != 0 && moveY != 0) {
+            const direction = getRandom(0, 2);
+            if (direction == 0) { this.x += moveX * 100; }
+            else { this.y += moveY * 100; }
+        }
+        this.element.style.top = this.y + 'px';
+        this.element.style.left = this.x + 'px';
+    }
+}
 
+const game = new Game(800, 600);
+game.render();
+
+const hero = new Hero(100, 500);
+hero.render();
+
+const treasure = new Treasure(400, 0);
+treasure.render();
+
+const ghost = new Ghost(600, 200);
+ghost.render();
 
 const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-const moveToHero = () => {
-    let moveX = 0;
-    let moveY = 0;
-    //move to hero by X
-    if (ghostX > heroX) {
-        moveX = -1;
-    }
-    else if (ghostX < heroX) {
-        moveX = 1;
-    }
-    else {
-        moveX = 0;
-    }
-    // move to hero by Y
-    if (ghostY > heroY) {
-        moveY = -1;
-    }
-    else if (ghostY < heroY) {
-        moveY = 1;
-    }
-    else {
-        moveY = 0;
-    }
-
-    // compare moveX and moveY
-    if (moveX == 0 && moveY != 0) {
-        ghostY += moveY * 100;
-    }
-    else if (moveX != 0 && moveY == 0) {
-        ghostX += moveX * 100;
-    }
-    else if (moveX != 0 && moveY != 0) {
-        const direction = getRandom(0, 2);
-        if (direction == 0) { ghostX += moveX * 100; }
-        else { ghostY += moveY * 100; }
-    }
-}
-
-const checkWin = () => {
-    if (treasureX == heroX && treasureY == heroY) {
-        const victory = document.querySelector('.victory');
-        victory.style.display = 'block';
-
-        gameStarted = false;
-        clearInterval(gameInterval);
-
-    }
-}
-
-const checkLose = () => {
-    if (ghostX == heroX && ghostY == heroY) {
-        const defeat = document.querySelector('.defeat');
-        defeat.style.display = 'block';
-
-        gameStarted = false;
-        clearInterval(gameInterval);
-    }
-}
-
 const gameInterval = setInterval(() => {
 
-    if (!gameStarted) { return; }
-    moveToHero();
+    if (!game.isStarted) { return; }
+    ghost.moveToHero(hero);
 
-    ghost.style.top = ghostY + 'px';
-    ghost.style.left = ghostX + 'px';
+    game.checkLose(ghost, hero, gameInterval);
+    game.checkWin(treasure, hero, gameInterval);
 
-    checkLose();
-    checkWin();
+}, 1200);
 
-}, 200);
+document.addEventListener('keyup', (e) => { hero.move(e.key, game, treasure, gameInterval) });
+
 
 //setTimeout(() => { clearInterval(intervalId) }, 5000)
 
